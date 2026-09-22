0d9ac1594fa8a353472646f093f7fc0835d8687f
Laboratório FIAP <labsfiap@fiap.com.br>
2026-09-22 20:19:05 -0300

    first commit

---
+++ app/src/main/java/boveri/com/github/todolist/MainActivity.kt
@@ -0,0 +1,25 @@
+package boveri.com.github.todolist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.lifecycle.viewmodel.compose.viewModel
+import boveri.com.github.todolist.navigation.AppNavigation
+import boveri.com.github.todolist.ui.theme.FiaptodolistTheme
+import boveri.com.github.todolist.viewmodel.TarefaViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FiaptodolistTheme {
+                val viewModel: TarefaViewModel = viewModel(
+                    factory = TarefaViewModel.factory(applicationContext)
+                )
+                AppNavigation(viewModel = viewModel)
+            }
+        }
+    }
+}
